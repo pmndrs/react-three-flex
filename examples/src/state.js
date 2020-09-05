939d75261e1a15ed8d0fb808c783758ca79b4b82
iinfin <48106228+iinfin@users.noreply.github.com>
2020-09-05 14:23:00 +0300

    change images

--- examples/src/state.js
+++ examples/src/state.js
@@ -1,27 +1,25 @@
-const pexels = (id) =>
-  `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
+const imgur = (id) => `https://i.imgur.com/${id}.jpg`
 
 const state = {
   top: 0,
   pages: 0,
   threshold: 4,
   content: [
-    { tag: '00', text: `The Bacchic\nand Dionysiac\nRites`, image1: pexels(911738), image2: pexels(358574) },
-    { tag: '01', text: `The Elysian\nMysteries`, image1: pexels(416430), image2: pexels(1738986) },
-    { tag: '02', text: `The Hiramic\nLegend`, image1: pexels(1005644), image2: pexels(327482) },
+    { tag: '00', text: `The Bacchic\nand Dionysiac\nRites`, image1: imgur('20xjASS'), image2: imgur('M2mxjL5') },
+    { tag: '01', text: `The Elysian\nMysteries`, image1: imgur('GfFfjb6'), image2: imgur('RzKRbxn') },
+    { tag: '02', text: `The Hiramic\nLegend`, image1: imgur('YWrVODD'), image2: imgur('DPDfTB9') },
   ],
-  depthimg:
-    'https://images.unsplash.com/photo-1500412830877-c77d92c33203?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=gian-d-YE6iUShVcps-unsplash.jpg&w=1920',
+  depthimg: imgur('jBx6WrQ'),
   depthbox: [
     {
       depth: 0,
       color: '#cccccc',
-      textColor: 'white',
+      textColor: '#ffffff',
       text: 'In a void,\nno one could say\nwhy a thing\nonce set in motion\nshould stop anywhere',
     },
     {
       depth: -6,
-      textColor: 'white',
+      textColor: '#0a0a0a',
       text:
         'For why should it stop\nhere rather than here?\nSo that a thing\nwill either be at rest\nor must be moved\nad infinitum',
     },
