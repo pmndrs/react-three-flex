baa20701b2ed981c3c93c9ecefec9d282af87ab0
iinfin <48106228+iinfin@users.noreply.github.com>
2020-09-06 00:25:17 +0300

    edit images

--- examples/src/state.js
+++ examples/src/state.js
@@ -11,8 +11,8 @@ const state = {
       text: `The Bacchic\nand Dionysiac\nRites`,
       images: [imgur('BH41NVu'), imgur('fBoIJLX'), imgur('04zTfWB')],
     },
-    { tag: '01', text: `The Elysian\nMysteries`, images: [imgur('BH41NVu'), imgur('fBoIJLX'), imgur('04zTfWB')] },
-    { tag: '02', text: `The Hiramic\nLegend`, images: [imgur('BH41NVu'), imgur('fBoIJLX'), imgur('04zTfWB')] },
+    { tag: '01', text: `The Elysian\nMysteries`, images: [imgur('c4cA8UN'), imgur('ajQ73ol'), imgur('gZOmLNU')] },
+    { tag: '02', text: `The Hiramic\nLegend`, images: [imgur('mbFIW1b'), imgur('mlDUVig'), imgur('gwuZrgo')] },
   ],
   depthbox: [
     {
@@ -20,14 +20,14 @@ const state = {
       color: '#cccccc',
       textColor: '#ffffff',
       text: 'In a void,\nno one could say\nwhy a thing\nonce set in motion\nshould stop anywhere',
-      image: imgur('BH41NVu')
+      image: imgur('mbFIW1b'),
     },
     {
       depth: -4.5,
       textColor: '#272727',
       text:
         'For why should it stop\nhere rather than here?\nSo that a thing\nwill either be at rest\nor must be moved\nad infinitum',
-      image: imgur('04zTfWB')
+      image: imgur('mbFIW1b'),
     },
   ],
 }
