c30456d92f69a7a83ffb02b6327655bb515fedfc
Paul Henschel <drcmda@gmail.com>
2020-09-07 02:28:45 +0200

    shrink build some more

--- examples/config-overrides.js
+++ examples/config-overrides.js
@@ -1,4 +1,11 @@
-const { override } = require('customize-cra')
+const { override, addWebpackAlias } = require('customize-cra')
 const { addReactRefresh } = require('customize-cra-react-refresh')
+const path = require('path')
 
-module.exports = override(addReactRefresh())
+module.exports = override(
+  addReactRefresh(),
+  addWebpackAlias({
+    three$: path.resolve('./src/utils/three.js'),
+    '../../../build/three.module.js': path.resolve('./src/utils/three.js'),
+  })
+)
