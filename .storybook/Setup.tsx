52249f0d8b2fb6c0367af2f6e560e1fbd4328dd9
Michael Bashurov <accounts@saitonakamura.me>
2021-08-11 12:31:13 +0300

    Added storybook and basic configuration and example

---
+++ .storybook/Setup.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import * as THREE from 'three'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+export function Setup({
+  children,
+  cameraFov = 75,
+  cameraPosition = new THREE.Vector3(0, 0, 5),
+  controls = true,
+  lights = false,
+  ...restProps
+}: {
+  children: React.ReactNode | React.ReactNode[]
+  cameraFov?: number
+  cameraPosition?: THREE.Camera['position']
+  controls?: boolean
+  lights?: boolean
+}) {
+  return (
+    <Canvas shadows camera={{ position: cameraPosition, fov: cameraFov }} dpr={window.devicePixelRatio} {...restProps}>
+      {children}
+      {lights && (
+        <>
+          <ambientLight intensity={0.8} />
+          <pointLight intensity={1} position={[0, 6, 0]} />
+        </>
+      )}
+      {controls && <OrbitControls />}
+    </Canvas>
+  )
+}
